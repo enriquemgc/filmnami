f103d19db5812679135c498bf1776063951fe555
enriquemgc <enriquemgc+dev@gmail.com>
2015-06-07 12:07:33 +0200

    add gulpfile.js and its dependecies
    Add gulp to handle project task. Node will reload when some file in
    “backend” directory has changed. This file will allow to add needed
    tasks like browse reload for a JS app.

---
+++ backend/gulpfile.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var gulp = require('gulp');
+var spawn = require('child_process').spawn;
+var node;
+
+
+
+// clean up if an error goes unhandled.
+process.on('exit', function() {
+    if (node) node.kill()
+});
+
+
+// reload node
+gulp.task('node', function() {
+    // if node is already running
+    if (node) node.kill();
+    // run node
+    node = spawn('node', ['app.js'], {stdio: 'inherit'});
+    // log on close
+    node.on('close', function (code) {
+      if (code === 8) {
+        gulp.log('Error detected, waiting for changes...');
+      }
+    });
+});
+
+
+// watch for changes in node related files. Reload node if needed
+gulp.task('default', ['node'], function() {
+
+  // files to watch
+  gulp.watch([
+    'backend/*',  // any file in backend folder
+    'backend/**/*.js',  // js files in directories inside backend 
+  ], ['node']); // tasks to run on file change
+
+});
